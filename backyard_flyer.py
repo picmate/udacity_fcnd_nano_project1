daf66c872061db095f9db0f59ed6f831e5955e70
Pathum Mudannayake <pmudannayake@skyhook.com>
2021-01-04 14:50:02 -0500

    Code cleanup and comments

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -1,3 +1,8 @@
+"""
+    Solution for Udacity Flying Car Nano Degree Project 1: Backyard Flyer
+    By Pathum Mudannayake
+"""
+
 import argparse
 import time
 from enum import Enum
@@ -37,8 +42,6 @@ class BackyardFlyer(Drone):
 
     def local_position_callback(self):
         """
-        TODO: Implement this method
-
         This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
         """
         if self.flight_state == States.TAKEOFF:
@@ -57,16 +60,12 @@ class BackyardFlyer(Drone):
 
     def velocity_callback(self):
         """
-        TODO: Implement this method
-
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
         pass
 
     def state_callback(self):
         """
-        TODO: Implement this method
-
         This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
         """
         if not self.in_mission:
@@ -82,21 +81,19 @@ class BackyardFlyer(Drone):
                 self.manual_transition()
 
     def calculate_box(self):
-        """TODO: Fill out this method
-        
+        """
         1. Return waypoints to fly a box
         """
-        box = [(10, 0, 3, 0),
-               (10, 10, 3, 0),
-               (0, 10, 3, 0),
-               (0, 0, 3, 0)]
+        box = [np.array([10, 0, 3]),
+               np.array([10, 10, 3]),
+               np.array([0, 10, 3]),
+               np.array([0, 0, 3])]
 
         for vertex in box:
             yield vertex
 
     def arming_transition(self):
-        """TODO: Fill out this method
-        
+        """
         1. Take control of the drone
         2. Pass an arming command
         3. Set the home location to current position
@@ -112,8 +109,7 @@ class BackyardFlyer(Drone):
         self.flight_state = States.ARMING
 
     def takeoff_transition(self):
-        """TODO: Fill out this method
-        
+        """
         1. Set target_position altitude to 3.0m
         2. Command a takeoff to 3.0m
         3. Transition to the TAKEOFF state
@@ -125,8 +121,7 @@ class BackyardFlyer(Drone):
         self.flight_state = States.TAKEOFF
 
     def waypoint_transition(self):
-        """TODO: Fill out this method
-    
+        """
         1. Command the next waypoint position
         2. Transition to WAYPOINT state
         """
@@ -136,13 +131,9 @@ class BackyardFlyer(Drone):
             next_vertex = next(self.all_waypoints)
             print('Next Waypoint: {}'.format(next_vertex))
 
-            target_north, target_east, target_altitude, target_heading = next_vertex
+            self.target_position = next_vertex
 
-            self.target_position[0] = target_north
-            self.target_position[1] = target_east
-            self.target_position[2] = target_altitude
-
-            self.cmd_position(target_north, target_east, target_altitude, target_heading)
+            self.cmd_position(self.target_position[0], self.target_position[1], self.target_position[2], 0.0)
 
             self.flight_state = States.WAYPOINT
 
@@ -150,8 +141,7 @@ class BackyardFlyer(Drone):
             self.landing_transition()
 
     def landing_transition(self):
-        """TODO: Fill out this method
-        
+        """
         1. Command the drone to land
         2. Transition to the LANDING state
         """
@@ -160,8 +150,7 @@ class BackyardFlyer(Drone):
         self.flight_state = States.LANDING
 
     def disarming_transition(self):
-        """TODO: Fill out this method
-        
+        """
         1. Command the drone to disarm
         2. Transition to the DISARMING state
         """
